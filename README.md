aaf61236c2ac454c621bd2205d5c74c71476f0b1
Ricky Elrod <ricky@elrod.me>
2016-05-22 15:15:14 -0700

    initial commit
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

---
+++ README.md
@@ -0,0 +1,7 @@
+# mapview-json
+
+An attempt to standardize JSON communication between mapview and frontends.
+
+# License
+
+MIT
---
+++ src/KD8ZRC/Mapview/Encoding/JSON.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module : KD8ZRC.Mapview.Encoding.JSON
+-- Copyright : (C) 2016 Ricky Elrod
+-- License : MIT (see LICENSE file)
+-- Maintainer : Ricky Elrod <ricky@elrod.me>
+-- Stability : experimental
+-- Portability : ghc
+--
+-- Provides an encoding for JSON encapsulation of mapview types.
+----------------------------------------------------------------------------
+module KD8ZRC.Mapview.Encoding.JSON
+       ( MapviewJson ()
+       , mkMapviewJson) where
+
+import qualified Control.Concurrent.Chan as Chan
+import Control.Monad (mzero)
+import Control.Monad.IO.Class
+import Data.Aeson
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.Text as T
+import KD8ZRC.Mapview.Types
+
+data MapviewJson t =
+  MapviewJson { mapviewJsonPacketType :: T.Text
+              , mapviewJsonData :: t
+              }
+
+mkMapviewJson :: ToJSON t => T.Text -> t -> MapviewJson t
+mkMapviewJson = MapviewJson
+
+instance ToJSON t => ToJSON (MapviewJson t) where
+    toJSON (MapviewJson pt d) =
+        object ["packet_type" .= pt, "data" .= d]
+
+instance FromJSON t => FromJSON (MapviewJson t) where
+    parseJSON (Object v) = MapviewJson <$> v .: "packet_type"
+                                       <*> v .: "data"
+    parseJSON _ = mzero
+
+-- | Write a JSON-encoded telemetry packet to a channel in the correct format.
+writeChanJsonPkt
+  :: ToJSON t
+  => Chan.Chan BS.ByteString
+  -> ParsedPacketCallback t
+writeChanJsonPkt ch =
+  ParseSuccessCallback (
+    \pkt -> liftIO $
+            Chan.writeChan ch (
+              BSL.toStrict . encode . mkMapviewJson "telemetry" $ pkt))
